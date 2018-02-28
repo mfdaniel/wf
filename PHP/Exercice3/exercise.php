1aa1e1fd90ee86d7679529b032b0305c51b96f2e
DanielMF <danielmartafernandes@gmail.com>
2018-02-28 15:18:22 +0100

    my solution

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,22 @@
 <?php
+var_dump($input);
 
-$input;
-$winner;
+$aResult = 0;
+$bResult = 0;
+
+foreach($input as $turn){
+    $cardA= $turn[0];
+    $cardB= $turn[1];
+    
+    if($cardA>$cardB){
+        $aResult++;
+    }else if($bResult>$cardA){
+        $bResult++;
+    }
+}  
+
+if($aResult>$bResult){
+    $winner = 'A';
+}else{
+    $winner = 'B'; 
+}
